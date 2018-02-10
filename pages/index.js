52b00b74242c30ed7f20891eaba90fa348c5205c
Ryan Lambrecht <rlambrecht09@gmail.com>
2018-02-10 11:11:16 -0600

    Added a Header component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Link from "next/link";
+import Header from "../components/Header";
 
 const Index = () => (
   <div>
-    <Link href="/about">
-      <button style={{ fontSize: 20 }}>About Page</button>
-    </Link>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 );
