f3f69b32688118f21142f3658b8e3339bd107956
Ryan Lambrecht <rlambrecht09@gmail.com>
2018-03-20 16:40:53 -0500

    server

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,60 @@
-import Layout from "../components/MyLayout";
+import Layout from "../components/MyLayout.js";
 import Link from "next/link";
+// isomorphic-unfetch is a fetch library that supports server and client fetching
+import fetch from "isomorphic-unfetch";
 
-// We are passing data via a query string parameter (a query param).
-// In our case, it's the “title” query param.
-
-// as does route masking for clean urls
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
-
-export default () => (
+// Style-jsx is recommended for the Next.js framework
+// However, CSS in JS might be more natural and comes with Syntax checking
+const Index = props => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      {props.shows.map(({ show }) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 );
+
+// getInitialProps gets data from a server to set as the props
+// fetches on the server and renders with the client
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
+};
+
+export default Index;
--- pages/post.js
+++ pages/post.js
@@ -1,16 +1,50 @@
-import Layout from "../components/MyLayout";
+import Layout from "../components/MyLayout.js";
+import fetch from "isomorphic-unfetch";
 
-// takes in the url query for title when at /post
-const Content = props => (
-  <div>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-);
-
-// The url prop is only expose to the default component
-export default props => (
+const Post = props => (
   <Layout>
-    <Content url={props.url} />
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
+    <img src={props.show.image.medium} />
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 );
+
+// Context is the url query
+// This renders on the client because we navigated to the
+// page on the client side. If you refreshed the page, it would
+// render on the server side.
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
